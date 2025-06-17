546d6200592693f44003972a1e23f8f27d6eee8a
Arthur Guerra <arthur.batista@sc.senai.br>
2025-06-17 00:06:29 -0300

    feat(auth): implement login logic in AuthService using AuthenticationManager and JwtUtil

--- src/main/java/com/arthur/urlshortener/auth/service/AuthService.java
+++ src/main/java/com/arthur/urlshortener/auth/service/AuthService.java
@@ -1,5 +1,7 @@
 package com.arthur.urlshortener.auth.service;
 
+import com.arthur.urlshortener.auth.dto.LoginRequest;
+import com.arthur.urlshortener.auth.dto.LoginResponse;
 import com.arthur.urlshortener.auth.dto.RegisterRequest;
 import com.arthur.urlshortener.auth.dto.RegisterResponse;
 import com.arthur.urlshortener.auth.entity.Email;
@@ -8,6 +10,11 @@ import com.arthur.urlshortener.auth.entity.Role;
 import com.arthur.urlshortener.auth.entity.User;
 import com.arthur.urlshortener.auth.exception.EmailAlreadyExistsException;
 import com.arthur.urlshortener.auth.repository.UserRepository;
+import com.arthur.urlshortener.auth.util.JwtUtil;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -18,10 +25,17 @@ public class AuthService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
 
-    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public AuthService(UserRepository userRepository,
+                       PasswordEncoder passwordEncoder,
+                       AuthenticationManager authenticationManager,
+                       JwtUtil jwtUtil) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
     }
 
     public RegisterResponse registerUser(RegisterRequest request) {
@@ -37,5 +51,18 @@ public class AuthService {
         userRepository.save(user);
 
         return new RegisterResponse("User " + request.email() + " registered successfully");
+    }
+
+
+    public LoginResponse login(LoginRequest request) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.email(), request.password())
+        );
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String token = jwtUtil.generateToken(userDetails.getUsername());
+        Long expiresIn = jwtUtil.getExpirationMs();
+
+        return new LoginResponse(token, "Bearer", expiresIn);
     }
 }
