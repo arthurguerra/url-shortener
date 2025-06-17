c88d1bf31d9aabc2b83027c743f51e32ab772a82
Arthur Guerra <arthur.batista@sc.senai.br>
2025-06-17 00:10:04 -0300

    feat(security): customize JWT auth failure response with 401 Unauthorized

---
+++ src/main/java/com/arthur/urlshortener/auth/filter/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,43 @@
+package com.arthur.urlshortener.auth.filter;
+
+import com.arthur.urlshortener.exception.ErrorResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
+
+    public JwtAuthenticationEntryPoint(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+
+
+        var error = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.UNAUTHORIZED.value(),
+                "Invalid Token",
+                authException.getMessage()
+        );
+
+        var json = objectMapper.writeValueAsString(error);
+        response.getWriter().write(json);
+    }
+}
