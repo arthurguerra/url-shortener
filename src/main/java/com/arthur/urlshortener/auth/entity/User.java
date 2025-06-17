566479c8ad1c4471e44443f0be6c805f684a9b3d
Arthur Guerra <arthur.batista@sc.senai.br>
2025-06-17 00:38:13 -0300

    feat(user): add getters and eager fetch for roles collection

--- src/main/java/com/arthur/urlshortener/auth/entity/User.java
+++ src/main/java/com/arthur/urlshortener/auth/entity/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -26,7 +27,7 @@ public class User {
     @Embedded
     private Password password;
 
-    @ElementCollection()
+    @ElementCollection(fetch = FetchType.EAGER)
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
 
@@ -37,5 +38,17 @@ public class User {
         this.email = email;
         this.password = password;
         this.roles = roles;
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+
+    public Password getPassword() {
+        return password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
     }
 }
